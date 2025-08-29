f027f0947094e467285ba55acfa585e4467d8487
Radheshyam kharol <73020015+radheshyamkharol121@users.noreply.github.com>
2025-08-29 21:08:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/',
+});
+
+export default defineConfig({
   plugins: [react()],
   build: {
     target: "esnext", // latest JS
