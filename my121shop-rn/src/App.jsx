f2bc1dcad5fa1a3f92e794aae00be0fc34f6cf84
Radheshyam kharol <73020015+radheshyamkharol121@users.noreply.github.com>
2025-08-29 21:29:05 +0530

    Update App.jsx

---
+++ my121shop-rn/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Product from './pages/Product';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<Product />} />
+      </Routes>
+    </Router>
+  );
+}
